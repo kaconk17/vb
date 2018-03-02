1d6ed8f02254cf5e3fff64f8593f89a9ba618f2c
kaconk uciha <harutonouciha@gmail.com>
2018-03-02 12:55:18 +0700

    update

--- tugas_array/Form1.Designer.vb
+++ tugas_array/Form1.Designer.vb
@@ -22,9 +22,109 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.txt_no = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.txt_nama = New System.Windows.Forms.TextBox()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.btn_simpan = New System.Windows.Forms.Button()
+        Me.btn_tampil = New System.Windows.Forms.Button()
+        Me.btn_clr = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'txt_no
+        '
+        Me.txt_no.Location = New System.Drawing.Point(95, 12)
+        Me.txt_no.Name = "txt_no"
+        Me.txt_no.Size = New System.Drawing.Size(60, 20)
+        Me.txt_no.TabIndex = 0
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(50, 15)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(38, 13)
+        Me.Label1.TabIndex = 1
+        Me.Label1.Text = "Nomer"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(50, 41)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(35, 13)
+        Me.Label2.TabIndex = 3
+        Me.Label2.Text = "Nama"
+        '
+        'txt_nama
+        '
+        Me.txt_nama.Location = New System.Drawing.Point(95, 38)
+        Me.txt_nama.Name = "txt_nama"
+        Me.txt_nama.Size = New System.Drawing.Size(133, 20)
+        Me.txt_nama.TabIndex = 2
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Location = New System.Drawing.Point(95, 139)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(120, 95)
+        Me.ListBox1.TabIndex = 4
+        '
+        'btn_simpan
+        '
+        Me.btn_simpan.Location = New System.Drawing.Point(113, 64)
+        Me.btn_simpan.Name = "btn_simpan"
+        Me.btn_simpan.Size = New System.Drawing.Size(75, 23)
+        Me.btn_simpan.TabIndex = 5
+        Me.btn_simpan.Text = "Simpan"
+        Me.btn_simpan.UseVisualStyleBackColor = True
+        '
+        'btn_tampil
+        '
+        Me.btn_tampil.Location = New System.Drawing.Point(113, 250)
+        Me.btn_tampil.Name = "btn_tampil"
+        Me.btn_tampil.Size = New System.Drawing.Size(75, 23)
+        Me.btn_tampil.TabIndex = 6
+        Me.btn_tampil.Text = "Tampil"
+        Me.btn_tampil.UseVisualStyleBackColor = True
+        '
+        'btn_clr
+        '
+        Me.btn_clr.Location = New System.Drawing.Point(10, 250)
+        Me.btn_clr.Name = "btn_clr"
+        Me.btn_clr.Size = New System.Drawing.Size(75, 23)
+        Me.btn_clr.TabIndex = 7
+        Me.btn_clr.Text = "Clear"
+        Me.btn_clr.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(304, 305)
+        Me.Controls.Add(Me.btn_clr)
+        Me.Controls.Add(Me.btn_tampil)
+        Me.Controls.Add(Me.btn_simpan)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.txt_nama)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.txt_no)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
     End Sub
 
+    Friend WithEvents txt_no As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents txt_nama As TextBox
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents btn_simpan As Button
+    Friend WithEvents btn_tampil As Button
+    Friend WithEvents btn_clr As Button
 End Class
--- tugas_array/Form1.vb
+++ tugas_array/Form1.vb
@@ -1,3 +1,38 @@
 ﻿Public Class Form1
+    Dim no(5) As Integer
+    Dim nama(5) As String
+    Dim i As Integer = 0
+    Private Sub btn_simpan_Click(sender As Object, e As EventArgs) Handles btn_simpan.Click
 
+        no(i) = txt_no.Text
+        nama(i) = txt_nama.Text
+        txt_no.Text = ""
+        txt_nama.Text = ""
+        If i < 5 Then
+
+
+            i = i + 1
+        Else
+            MsgBox("Data Penuh", MsgBoxStyle.Critical)
+        End If
+
+    End Sub
+
+    Private Sub btn_tampil_Click(sender As Object, e As EventArgs) Handles btn_tampil.Click
+        Array.Sort(no, nama)
+
+        Dim p As Integer = 5
+        For a As Integer = i To 1 Step -1
+
+            ListBox1.Items.Add(no(p) & " " & nama(p))
+            p = p - 1
+        Next
+    End Sub
+
+    Private Sub btn_clr_Click(sender As Object, e As EventArgs) Handles btn_clr.Click
+        ListBox1.Items.Clear()
+        i = 0
+        Array.Clear(no, 0, no.Length)
+        Array.Clear(nama, 0, nama.Length)
+    End Sub
 End Class
